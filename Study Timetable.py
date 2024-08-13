372eddbc71bc16eaf878d36d4e747972df877dec
Aahan-dev <129125344+Aahan-dev@users.noreply.github.com>
2024-08-13 21:13:26 +1000

    Finished Study Timetable

--- Study Timetable.py
+++ Study Timetable.py
@@ -34,3 +34,47 @@ class StudyTimetable:
             else:
                 print("  No activities scheduled.")
 
+def main():# Main function to run the study timetable application.
+   
+    timetable = StudyTimetable()  # Create an instance of StudyTimetable
+
+
+    while True:
+        print("\nStudy Timetable")
+        print("1. Add Activity")
+        print("2. Remove Activity")
+        print("3. View Schedule")
+        print("4. Exit")
+
+
+        choice = input("Choose an option: ")
+
+
+        if choice == '1':
+            day = input("Enter the day of the week: ")
+            time = input("Enter the time (e.g., '10:00 AM'): ")
+            activity = input("Enter the activity: ")
+            timetable.add_activity(day, time, activity)  # Add a new activity
+
+
+        elif choice == '2':
+            day = input("Enter the day of the week: ")
+            time = input("Enter the time of the activity to remove: ")
+            timetable.remove_activity(day, time)  # Remove an activity
+
+
+        elif choice == '3':
+            timetable.view_schedule()  # View the complete schedule
+
+
+        elif choice == '4':
+            print("Exiting the Study Timetable application. Goodbye!")
+            break  # Exit the loop
+
+
+        else:
+            print("Invalid option. Please choose again.")
+
+
+if __name__ == "__main__":
+    main()  # Run the main function
